7e3bbfda627a5d47e8a9023602b41ca202ebbcd9
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-02 07:42:32 +0300

    access key placed inside environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  token: '45c1c29cf9830ac2e40089b549893fb070a6bf69',
 };
 
 /*
