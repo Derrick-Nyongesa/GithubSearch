f85013fbfde9f351f6df2b2825a40da64a199459
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-02 19:22:48 +0300

    added an alert method

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent},
-  { path: 'repositories', component: RepositoriesComponent}
+  { path: 'repositories', component: RepositoriesComponent},
+  { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
